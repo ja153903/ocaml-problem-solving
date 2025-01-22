7ab299d72bc835c46212390d9b51d41ad2512fca
ja153903 <jaimeabbariao@gmail.com>
2025-01-21 22:19:05 -0500

    updating with some values

--- lib/intermediate.ml
+++ lib/intermediate.ml
@@ -176,3 +176,22 @@ let%test _ =
   rotate [ "a"; "b"; "c"; "d"; "e"; "f"; "g"; "h" ] 3
   = [ "d"; "e"; "f"; "g"; "h"; "a"; "b"; "c" ]
 ;;
+
+(* extract a given number of randomly selected elements from a list *)
+let rand_select list n =
+  Random.init 0;
+  let rec extract acc n = function
+    | [] -> raise Not_found
+    | h :: t -> if n = 0 then h, acc @ t else extract (h :: acc) (n - 1) t
+  in
+  let extract_rand list len = extract [] (Random.int len) list in
+  let rec aux n acc list len =
+    if n = 0
+    then acc
+    else (
+      let picked, rest = extract_rand list len in
+      aux (n - 1) (picked :: acc) rest (len - 1))
+  in
+  let len = List.length list in
+  aux (min n len) [] list len
+;;
